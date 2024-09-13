e4d0e16dfb8c7aee7599abdd6eb7380f9fbdc710
portellam <amportell.13@gmail.com>
2024-09-13 03:15:04 -0400

    Update.

--- installer.sh
+++ installer.sh
@@ -22,23 +22,41 @@ function install
 {
   local -r relative_path="bin"
   local -r script_file="parse-iommu-devices"
+  local -r parent_path="/usr/local/bin/"
+  local -r binary_path="${parent_path}/${script_file}.d/"
 
-  if [[ ! -e "${relative_path}/${script_file}" ]]; then
-    echo "Error: Cannot locate script file."
+  if ! mkdir --parents "${binary_path}" &> /dev/null; then
+    echo "Error: Cannot create script directory '${binary_path}'."
     return 1
   fi
 
-  local -r destination_path="/usr/local/bin"
-
-  if ! cp --force --recursive "${relative_path}/${script_file}" \
-    "${destination_path}/${script_file}" &> /dev/null; then
-    echo "Error: Cannot copy script file."
+  if ! install_this "${script_file}" "${parent_path}" \
+    || ! install_this "datatype_src" "${binary_path}" \
+    || ! install_this "parse_src" "${binary_path}" \
+    || ! install_this "print_src" "${binary_path}" \
+    || ! install_this "xml_src" "${binary_path}"; then
     return 1
   fi
 
-  if ! chmod u+x "${destination_path}/${script_file}" \
+  return 0
+}
+
+function install_this
+{
+  if [[ ! -e "${relative_path}/${1}" ]]; then
+    echo "Error: Cannot locate script file '${1}'."
+    return 1
+  fi
+
+  if ! cp --force --recursive "${relative_path}/${1}" \
+    "${2}${1}" &> /dev/null; then
+    echo "Error: Cannot copy script file '${1}'."
+    return 1
+  fi
+
+  if ! chmod u+x "${2}${1}" \
     &> /dev/null; then
-    echo "Error: Cannot set script file as executable."
+    echo "Error: Cannot set script file '${1}' as executable."
     return 1
   fi
 
